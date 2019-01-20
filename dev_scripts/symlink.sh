0397393579d0b6499b7ab626e2fe52b8c4448dd5
dakanji <dakanji@user.noreply.github.com>
2019-01-20 23:00:42 +0300

    Update Symlink Script

--- dev_scripts/symlink.sh
+++ dev_scripts/symlink.sh
@@ -2,12 +2,9 @@
 
 if [ ! -L .git/hooks ];
 then
-	echo ".git/hooks is not symlink"
-	echo "copying .git/hooks to .git/old_hooks"
+	echo "copying '.git/hooks' to '.git/old_hooks'"
 	mv .git/hooks .git/old_hooks
 
-	echo "symlinking ../dev_scripts/git-hooks .git/hooks"
+	echo "symlinking '.git/hooks' to '/dev_scripts/git-hooks'"
 	ln -s ../dev_scripts/git-hooks .git/hooks
-else
-	echo ".git/hooks is already a symlink"
 fi
