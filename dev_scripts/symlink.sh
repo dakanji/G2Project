d0e9be2c689a6c3c63c7f93b79634fd21feff669
dakanji <dakanji@user.noreply.github.com>
2019-01-23 22:33:10 +0300

    Update Developer Environment

--- dev_scripts/symlink.sh
+++ dev_scripts/symlink.sh
@@ -5,6 +5,6 @@ then
 	echo "copying '.git/hooks' to '.git/old_hooks'"
 	mv .git/hooks .git/old_hooks
 
-	echo "symlinking '.git/hooks' to '/dev_scripts/git-hooks'"
-	ln -s ../dev_scripts/git-hooks .git/hooks
+	echo "symlinking '.git/hooks' to '/dev_scripts/hooks'"
+	ln -s ../dev_scripts/hooks .git/hooks
 fi
