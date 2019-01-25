ae7d3f2af20e6a6be91de101c60a9b5a0d4bac85
dakanji <dakanji@user.noreply.github.com>
2019-01-25 17:55:01 +0300

    Update Developer Environment

--- dev_scripts/setup.sh
+++ dev_scripts/setup.sh
@@ -2,8 +2,11 @@
 
 if [ ! -L .git/hooks ];
 then
-	echo "copying '.git/hooks' to '.git/old_hooks'"
-	mv .git/hooks .git/old_hooks
+	if [ -x .git/hooks ];
+	then
+		echo "copying '.git/hooks' to '.git/old_hooks'"
+		mv .git/hooks .git/old_hooks
+	fi
 
 	echo "symlinking '.git/hooks' to '/dev_scripts/hooks'"
 	ln -s ../dev_scripts/hooks .git/hooks
